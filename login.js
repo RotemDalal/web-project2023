3f5e6dcc9ab3bf12bad2d94af843c3d43cf374e2
Linoy <linoy2209@gmail.com>
2023-08-24 23:34:18 +0300

    fixing login and register

--- login.js
+++ login.js
@@ -1,12 +1,3 @@
-function openHamburger() {
-    if ($('.hamburger-menu').css('display') === 'none') {
-        $('.hamburger-menu').show();
-    } else {
-        $('.hamburger-menu').hide();
-    }
-}
-
-
 $('#register-item').click(() => {
     $('#modal-register').show();
 })
@@ -91,6 +82,7 @@ $('#login-item').click(function () {
 
 $('#cancel-btn').click(() => {
     $('#modal-login').hide();
+    history.back();
 })
 
 $('#login-btn').click(() => {
@@ -136,93 +128,5 @@ $('#login-btn').click(() => {
         localStorage.setItem("user", JSON.stringify(response));
         $('#hello-nav').text("hello " + response.username);
     });
+    
 })
-
-// https://swiperjs.com/get-started
-$(document).ready(function () {
-    new Swiper('.swiper', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        loop: true,
-        loopedSlides: 10,
-        slidesPerView: 'auto',
-        spaceBetween: 150,
-        centeredSlides: true,
-        simulateTouch: true,
-        touchEventsTarget: true,
-        touchReleaseOnEdges: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-
-    })
-})
-
-
-// Animation
-AOS.init({
-    duration: 1400,
-    once: true,
-    // delay: 400,
-});
-
-/*function validateForm() {
-  
-    let email = document.forms["myForm"]["email"].value;
-    if (email === "") {
-        alert("Email address must be filled out");
-        return false;
-    }
-    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-        alert("You have entered an invalid email address");
-        return false;
-    }
-    alert("Your details have been sent successfully!");
-    return true;
-}*/
-
-function switchColor(id) {
-    let val = document.getElementById(id).innerHTML;
-    if (val === "Select") {
-        document.getElementById(id).innerHTML = 'Unselect';
-        document.getElementById(id).style.backgroundColor = '#eee';
-        document.getElementById(id).style.border = '#eee';
-        document.getElementById(id).style.color = 'black';
-    }
-    if (val === "Unselect") {
-        document.getElementById(id).innerHTML = 'Select';
-        document.getElementById(id).style.backgroundColor = '#282828';
-        document.getElementById(id).style.border = '#282828';
-        document.getElementById(id).style.color = 'white';
-    }
-}
-
-function makeOrder() {
-    let counter = 0;
-    let numOfCards = document.getElementsByClassName('prod-card').length;
-    for (let i = 1; i <= numOfCards; i++) {
-        let val = document.getElementById(i).innerHTML;
-        if (val === "Unselect") {
-            counter++;
-            document.getElementById(i).innerHTML = 'Select';
-            document.getElementById(i).style.backgroundColor = '#282828';
-            document.getElementById(i).style.border = '#282828';
-            document.getElementById(i).style.color = 'white';
-        }
-    }
-    if (counter === 0) {
-        alert("No product selected");
-    }
-    else if (counter === 1) {
-        alert("Your order has been accepted! (" + counter + " product total)");
-    } else {
-        alert("Your order has been accepted! (" + counter + " products total)");
-    }
-}
