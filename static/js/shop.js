1cedf39736be7fdf9d39a96f93bb1c66b3150fd2
Rotem Dalal <rotemdalal254@gmail.com>
2023-09-26 12:35:24 +0300

    Small cleanup

--- static/js/shop.js
+++ static/js/shop.js
@@ -6,7 +6,7 @@ function updatemultiBy(num) {
 
 async function getProducts() {
   try {
-    const response = await fetch('http://localhost:5500/products'); // Replace with your API endpoint
+    const response = await fetch('/products'); // Replace with your API endpoint
     if (!response.ok) {
       throw new Error('Failed to fetch products');
     }
